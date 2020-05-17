6ed13aa9552cbe69d739df025c2aadd646d6d897
Salim Chehab <salimchehab@gmail.com>
2020-05-17 15:59:38 +0200

    Refactors tf aws plugin name

--- provision.sh
+++ provision.sh
@@ -3,12 +3,12 @@
 readonly home_dir_vagrant="/home/vagrant"
 readonly SALT_VERSION="2017.7.4+dfsg1-1ubuntu18.04.1"
 readonly TERRAFORM_VERSION="0.12.25"
-readonly TERRAFORM_PROVIDER_VERSION="2.62.0"
+readonly TERRAFORM_AWS_PROVIDER_VERSION="2.62.0"
 readonly VAULT_VERSION="1.4.1"
 readonly TERRAFORM_ZIP_FILE="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 readonly TERRAFORM_DOWNLOAD_LINK="https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/$TERRAFORM_ZIP_FILE"
-readonly TERRAFORM_PROVIDER_ZIP_FILE="terraform-provider-aws_${TERRAFORM_PROVIDER_VERSION}_linux_amd64.zip"
-readonly TERRAFORM_PROVIDER_DOWNLOAD_LINK="https://releases.hashicorp.com/terraform-provider-aws/${TERRAFORM_PROVIDER_VERSION}/${TERRAFORM_PROVIDER_ZIP_FILE}"
+readonly TERRAFORM_AWS_PROVIDER_ZIP_FILE="terraform-provider-aws_${TERRAFORM_AWS_PROVIDER_VERSION}_linux_amd64.zip"
+readonly TERRAFORM_AWS_PROVIDER_DOWNLOAD_LINK="https://releases.hashicorp.com/terraform-provider-aws/${TERRAFORM_AWS_PROVIDER_VERSION}/${TERRAFORM_AWS_PROVIDER_ZIP_FILE}"
 readonly VAULT_ZIP_FILE="vault_${VAULT_VERSION}_linux_amd64.zip"
 readonly VAULT_DOWNLOAD_LINK="https://releases.hashicorp.com/vault/${VAULT_VERSION}/${VAULT_ZIP_FILE}"
 # plugin locations: ~/.terraform.d/plugin or ~/.terraform.d/plugins/<OS>_<ARCH>
@@ -56,9 +56,9 @@ function configure_binaries() {
 
   # get Terraform AWS plugin
   mkdir -p "${TERRAFORM_PLUGIN_DIR}" &&
-    wget --quiet --directory-prefix="${TERRAFORM_PLUGIN_DIR}" "${TERRAFORM_PROVIDER_DOWNLOAD_LINK}" &&
-    unzip -o "${TERRAFORM_PLUGIN_DIR}/${TERRAFORM_PROVIDER_ZIP_FILE}" -d "${TERRAFORM_PLUGIN_DIR}" &&
-    rm "${TERRAFORM_PLUGIN_DIR}/${TERRAFORM_PROVIDER_ZIP_FILE}"
+    wget --quiet --directory-prefix="${TERRAFORM_PLUGIN_DIR}" "${TERRAFORM_AWS_PROVIDER_DOWNLOAD_LINK}" &&
+    unzip -o "${TERRAFORM_PLUGIN_DIR}/${TERRAFORM_AWS_PROVIDER_ZIP_FILE}" -d "${TERRAFORM_PLUGIN_DIR}" &&
+    rm "${TERRAFORM_PLUGIN_DIR}/${TERRAFORM_AWS_PROVIDER_ZIP_FILE}"
 
   # get Vault
   wget --quiet --directory-prefix="/tmp" "${VAULT_DOWNLOAD_LINK}" &&
